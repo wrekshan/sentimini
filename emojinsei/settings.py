dbe7668e3f77488d45c939519882b0db31890698
William Rekshan <williamrekshan@myhost.local>
2016-06-15 15:12:12 -0700

    first

--- emojinsei/settings.py
+++ emojinsei/settings.py
@@ -212,7 +212,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') #don't know what t
 
 #These were different than static
 ALLOWED_HOSTS = [] 
-DEBUG = False
+DEBUG = True
 
 #if i include this, then i don't get errors with collecting static
 # try:
