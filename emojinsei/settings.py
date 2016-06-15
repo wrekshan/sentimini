190e90b9703006377454ae4434ae9accac7a2dcf
William Rekshan <williamrekshan@myhost.local>
2016-06-15 14:43:05 -0700

    first

--- emojinsei/settings.py
+++ emojinsei/settings.py
@@ -197,4 +197,15 @@ CELERY_RESULT_BACKEND = 'djcelery.backends.database:DatabaseBackend'
 
 ####### DJ GIRLS TOLD ME TO ADD THIS:
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
