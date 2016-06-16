4daffeeb918c546b092eb6d79f9cc5b58e0001f2
William Rekshan <williamrekshan@myhost.local>
2016-06-16 12:00:56 -0700

    first

--- emojinsei/settings.py
+++ emojinsei/settings.py
@@ -217,8 +217,8 @@ DATABASES = {
 }
 
 #### PRODUCTION
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 # DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') #don't know what this line is doing
 
