9815e6b78beb5496e515fbb89c880c56bf31f203
William Rekshan <williamrekshan@myhost.local>
2016-06-16 09:03:02 -0700

    first

--- emojinsei/settings.py
+++ emojinsei/settings.py
@@ -144,6 +144,12 @@ STATIC_URL = '/static/'
 #     os.path.join(PROJECT_ROOT, 'static'),
 # )
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
 ######## THIS IS FROM THE HEROKU DOCS
 
 
