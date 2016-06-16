d6c960db8066df9c1e26ed4db554705c3a1947d1
William Rekshan <williamrekshan@myhost.local>
2016-06-16 15:32:43 -0700

    first

--- emojinsei/settings.py
+++ emojinsei/settings.py
@@ -133,9 +133,9 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
