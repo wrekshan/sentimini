a5a4a17110085a47bce477b37ea35ba58000bb2a
William Rekshan <williamrekshan@myhost.local>
2016-06-16 16:53:52 -0700

    first

--- emojinsei/celery.py
+++ emojinsei/celery.py
@@ -1,25 +1,26 @@
-# from __future__ import absolute_import
+from __future__ import absolute_import
 
-# import os
+import os
 
-# from celery import Celery
+from celery import Celery
 
-# # set the default Django settings module for the 'celery' program.
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emojinsei.settings')
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emojinsei.settings')
 
-# from django.conf import settings
+from django.conf import settings
 
-# app = Celery('emojinsei',
-#              broker='amqp://',
-#              backend='amqp://',
-#              include=['emojinsei.tasks'])
+app = celery.Celery('emojinsei')
 
-# # Optional configuration, see the application user guide.
-# app.conf.update(
-#     CELERY_TASK_RESULT_EXPIRES=3600,
-# )
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
 
 
+# Optional configuration, see the application user guide.
+app.conf.update(
+    CELERY_TASK_RESULT_EXPIRES=3600,
+)
 
-# if __name__ == '__main__':
-#     app.start()
+
+
+if __name__ == '__main__':
+    app.start()
