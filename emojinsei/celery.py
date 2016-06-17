ed232cf5f1bf506aad590be9a133d538934d33da
William Rekshan <williamrekshan@myhost.local>
2016-06-16 17:14:20 -0700

    first

--- emojinsei/celery.py
+++ emojinsei/celery.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 import os
 
-from celery import Celery
+import celery
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emojinsei.settings')
