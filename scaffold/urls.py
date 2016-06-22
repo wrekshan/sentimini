c9cac160b8a2e9b70df1af762955f95f3248abdf
William Rekshan <williamrekshan@myhost.local>
2016-06-22 12:14:52 -0700

    first

--- scaffold/urls.py
+++ scaffold/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import faq
+from .views import faq, detail
 
 urlpatterns = [
 	url(r'^faq/$', faq, name='faq'),
+	url(r'^faq/(?P<question_id>[0-9]+)/$', detail, name='detail'),
 ]
--- scaffold/views.py
+++ scaffold/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib import messages
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
 from .forms import  FAQuserquestionsForm
@@ -24,6 +24,36 @@ def faq(request):
 
 		
 		context = {
+			"faq_usage": faq_usage,
+			"faq_concept": faq_concept,
+			"form": form,
+		}			
+
+		return render(request,"faq.html",context)
+	else:
+		return HttpResponseRedirect('/accounts/signup/')
+
+
+def detail(request, question_id):
+	if request.user.is_authenticated():	
+		#don't forget to add form for user generated question
+		faq_usage = FAQ.objects.all().filter(category="Usage")
+		faq_concept = FAQ.objects.all().filter(category="Concept")
+		faq_detail = FAQ.objects.all().get(id=question_id)
+
+		if request.method == "POST":
+			form = form = FAQuserquestionsForm(request.POST)
+			if form.is_valid():
+				messages.add_message(request, messages.INFO, 'Question added!')	
+				working_faq = form.save(commit=False)
+				FAQuserquestions(user=request.user,question=working_faq.question).save()
+				return HttpResponseRedirect(reverse('scaffold:faq'))
+		else:
+			form = FAQuserquestionsForm()
+
+		
+		context = {
+			"faq_detail": faq_detail,
 			"faq_usage": faq_usage,
 			"faq_concept": faq_concept,
 			"form": form,
