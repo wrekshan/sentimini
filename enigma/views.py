bd7021f26971db65123c626dc4d5d90bbb7e0f07
William Rekshan <williamrekshan@myhost.local>
2016-11-08 11:18:13 -0800

    “first”

--- enigma/views.py
+++ enigma/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Make the same request we did earlier, but with the coordinates of San Francisco instead.
+# parameters = {"lat": 37.78, "lon": -122.41}
+# response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
