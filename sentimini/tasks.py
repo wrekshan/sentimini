6587a9a62faad1b9888b3e3c8e7b0915d5a229f9
wrekshan <william.rekshan@gmail.com>
2017-05-30 21:40:30 -0700

    master

--- sentimini/tasks.py
+++ sentimini/tasks.py
@@ -34,22 +34,22 @@ from .celery import app
 app.conf.beat_schedule = {
     'schedule': {
         'task': 'schedule_texts',
-        'schedule': timedelta(seconds=5),
+        'schedule': timedelta(seconds=10),
         'args': ()
     },
     'send': {
         'task': 'send_texts',
-        'schedule': timedelta(seconds=5),
+        'schedule': timedelta(seconds=10),
         'args': ()
     },
     'check': {
         'task': 'check_email_for_new',
-        'schedule': timedelta(seconds=5),
+        'schedule': timedelta(seconds=10),
         'args': ()
     },
     'process': {
         'task': 'process_new_mail',
-        'schedule': timedelta(seconds=5),
+        'schedule': timedelta(seconds=10),
 		'args': ()
     },
 }    
