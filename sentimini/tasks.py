b60b804d37e5e06ad950e90af176a3a62e4710f8
wrekshan <william.rekshan@gmail.com>
2017-06-20 10:06:50 -0700

    master

--- sentimini/tasks.py
+++ sentimini/tasks.py
@@ -124,7 +124,7 @@ def get_sun_time(sundata,desired):
 		if sundata[i]['phen'] == desired:
 			return sundata[i]['time']
 
-@task(name='schedule_sun_texts',rate_limit="1/h")	
+@task(name='schedule_sun_texts',rate_limit="4/h")	
 def schedule_sun_texts():
 	print("SUUUUUUUUNNNNNNN")
 	date_now = str(datetime.now(pytz.utc).strftime('%-m/%-d/%Y'))
@@ -175,7 +175,7 @@ def schedule_sun_texts():
 						atext.save()
 
 
-@task(name='schedule_moon_texts',rate_limit="1/h")
+@task(name='schedule_moon_texts',rate_limit="4/h")
 def schedule_moon_texts():
 	print("MOOOOOOOOON")
 	import requests
